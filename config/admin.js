a2c1a3f8f2e39dd2435c835e857ee51c7dfd099b
Geoff <geoff.ijamieson@gmail.com>
2023-04-29 19:18:22 -0400

    j

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,17 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
+  auditLogs: {
+    // only accessible with an Enterprise plan
+    enabled: env.bool("AUDIT_LOGS_ENABLED", true),
+  },
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
+  },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT"),
+    },
   },
 });

e51db9a7729c24066309658053df9c6e3482ba71
Geoff <geoff.ijamieson@gmail.com>
2023-04-29 19:09:37 -0400

    ready to deploy to render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,21 +1,6 @@
 module.exports = ({ env }) => ({
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
   url: env("RENDER_EXTERNAL_URL"),
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "someRandomLongString"),
-  },
-  auditLogs: {
-    // only accessible with an Enterprise plan
-    enabled: env.bool("AUDIT_LOGS_ENABLED", true),
-  },
-  auth: {
-    secret: env("ADMIN_JWT_SECRET", "someSecretKey"),
-  },
-  transfer: {
-    token: {
-      salt: env("TRANSFER_TOKEN_SALT", "anotherRandomLongString"),
-    },
+  dirs: {
+    public: "/data/public",
   },
 });

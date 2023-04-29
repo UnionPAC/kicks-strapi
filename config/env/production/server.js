7eb6b5dbac70d5ddfbf65f86c1392fc45bbc731e
Geoff <geoff.ijamieson@gmail.com>
2023-04-29 18:55:53 -0400

    r

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,21 @@
 module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "hP0Vl2rc9OFZP1nZ5n17aTDfQqdDHX"),
-  },
   app: {
     keys: env.array("APP_KEYS"),
   },
   url: env("RENDER_EXTERNAL_URL"),
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "someRandomLongString"),
+  },
+  auditLogs: {
+    // only accessible with an Enterprise plan
+    enabled: env.bool("AUDIT_LOGS_ENABLED", true),
+  },
+  auth: {
+    secret: env("ADMIN_JWT_SECRET", "someSecretKey"),
+  },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT", "anotherRandomLongString"),
+    },
+  },
 });
